145cfb69dc8bec5f1f660c342cc4fd6138ce80ec
Matthias Abddisa <matthiasabddisa@gmail.com>
2024-04-01 11:05:17 +0300

    modified:   config/database.php 	modified:   database/factories/UserFactory.php 	modified:   database/seeders/DatabaseSeeder.php 	new file:   database/whatsapp.db 	new file:   whatsapp.db

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('Jochebed@4ever'),
             'remember_token' => Str::random(10),
         ];
     }
