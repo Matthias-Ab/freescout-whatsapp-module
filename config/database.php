8a66eb6acef0ff941eff1e39e2d8758f250eccd0
Matthias Abddisa <matthiasabddisa@gmail.com>
2024-04-01 12:01:45 +0300

    modified:   config/database.php

--- config/database.php
+++ config/database.php
@@ -41,12 +41,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('whatsapp.db'),
+            'url' => env(''),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'whatsapp'),
-            'username' => env('DB_USERNAME', 'matthias'),
-            'password' => env('DB_PASSWORD', ''),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'Jochebed'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
